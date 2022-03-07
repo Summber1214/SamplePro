33b24c1169207aff802d6b8a6b1d7de6e1928095
HyeonGyu <marrin1101@gmail.com>
2022-03-07 12:27:18 +0900

    컴ㅅ

--- src/GitTestMain.java
+++ src/GitTestMain.java
@@ -4,9 +4,12 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		System.out.println("GitTestMain");
 		
+		
+		
 		System.out.println("hotfix branch");
 		System.out.println("hotfix branch2");
 
+		System.out.println("Master branch2");
 	}
 
 }
